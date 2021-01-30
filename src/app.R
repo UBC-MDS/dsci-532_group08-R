f26d15118f0f5d656c4e0a01ece193d5c0f77d5f
jordanlau123 <jordannlau@gmail.com>
2021-01-29 19:24:18 -0800

    updates on dashboard

--- src/app.R
+++ src/app.R
@@ -6,19 +6,26 @@ library(tidyverse)
 library(plotly)
 library(here)
 
-data_raw <- read_csv(here('data', 'processed.csv')) %>%
+data_raw <- read_csv('~/dsci-532_group08-R/data/processed.csv') %>%
   mutate(work_interfere = factor(work_interfere, levels = c('Often', 'Sometimes', 'Rarely', 'Never')))
 
 app <- Dash$new(external_stylesheets = dbcThemes$BOOTSTRAP)
 
+#plot layout
 plots_general_overview <- dbcRow(list(
   dbcCol(list(
-    dbcRow(dccGraph(id = 'plot-bar1')),
+    dbcRow(dccGraph(id = 'plot-hist')),
     dbcRow(dccGraph(id = 'plot-bar2'))
-  ), md = 4),
-  dbcCol(dccGraph(id = 'plot-map'))
-))
+  ), md = 5),
+  dbcCol(list(
+    dbcRow(dccGraph(id = 'plot-map')),
+    dbcRow(dccGraph(id = 'plot-bar1'))
+), md =5)))
 
+
+
+
+#General Tab: 1st row: filter selections, 2nd row: plots
 tab_general_overview_content <- dbcCard(
   dbcCardBody(list(
     dbcRow(list(
@@ -59,6 +66,7 @@ tab_general_overview_content <- dbcCard(
   ))
 )
 
+#title and page tabs 
 app$layout(dbcContainer(list(
   dbcRow(htmlH2('Mental Health in Tech Dashboard'), justify = 'center'),
   dbcRow(list(
@@ -79,10 +87,12 @@ app$layout(dbcContainer(list(
 ), fluid = T, style = list('border-width' = '10')))
 
 
+#plots
 app$callback(
   list(
     output('plot-bar1', 'figure'),
     output('plot-bar2', 'figure'),
+    output("plot-hist", 'figure'),
     output('plot-map', 'figure')
   ),
   list(
@@ -115,31 +125,37 @@ app$callback(
     }
 
     p_wi <- data_filtered %>% ggplot(aes(y = work_interfere)) +
-      geom_bar(stat = 'count') +
-      labs(x = 'Count of Records', y = 'Work Interfere',
-           title = 'Work interference')
+      geom_bar(stat = 'count', fill = "#99ccff", width = 0.8) +
+      labs(x = 'Count of Records', y = '',title = 'Work interference')+
+      theme_classic()
 
     p_wp <- data_filtered %>%
       add_count(wellness_program) %>%
       ggplot(aes(y = reorder(wellness_program, -n))) +
-      geom_bar() +
-      ylab('') +
-      ggtitle('Has your employer ever discussed mental health as part of an employee wellness program?') +
-      theme(
-        text = element_text(size=10)
-      )
+      geom_bar(fill = "#99ccff") +
+      labs(y= '', x= "Count of records") +
+      ggtitle('Has your employer ever discussed mental health \r\n as part of an employee wellness program?') +
+      theme(text = element_text(size=10))+
+      theme_classic()
 
-
+    p_ad <- data_filtered %>% 
+      ggplot(aes(y= Age))+
+      geom_histogram(fill = "#99ccff")+
+      labs(y= "Age", x= "Count of records", title= "Age Distribution")+
+      theme_classic()
+    
     data_grouped <- data_filtered %>%
       group_by(state) %>%
       tally()
-    p_map <- plot_ly(data_grouped, type = 'choropleth', locationmode = 'USA-states', locations = ~state, z = ~n) %>%
+    p_map <- plot_ly(data_grouped, type = 'choropleth', locationmode = 'USA-states', locations = ~state, z = ~n, 
+                     colors=c("#f0f9e8","#bae4bc","#7bccc4","#43a2ca","#0868ac")) %>%
       layout(geo = list(scope = 'usa', projection = list(type = 'albers usa')),
              title = 'Count of records by state')
 
     list(ggplotly(p_wi),
          ggplotly(p_wp),
-         subplot(p_map, ggplotly(p_wi)))
+         ggplotly(p_ad),
+         ggplotly(p_map))
   }
 )
 
